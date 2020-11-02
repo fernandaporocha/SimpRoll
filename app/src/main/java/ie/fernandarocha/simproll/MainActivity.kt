d2547865c244eba5d5c4441b96b6ca86c1e0c0b4
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-02 14:15:50 +0000

    Finding view efficiently

--- app/src/main/java/ie/fernandarocha/simproll/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/simproll/MainActivity.kt
@@ -9,9 +9,12 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
     }
