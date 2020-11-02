29b9a3d8cc80e4af6941aa79b0d29d881d54ef34
Fernanda Rocha <fernandaporocha@gmail.com>
2020-11-02 12:17:45 +0000

    Adding the Count Up button and functionality

--- app/src/main/java/ie/fernandarocha/simproll/MainActivity.kt
+++ app/src/main/java/ie/fernandarocha/simproll/MainActivity.kt
@@ -13,11 +13,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+        val countUpButton: Button = findViewById(R.id.countup_button)
+        countUpButton.setOnClickListener{ countUp() }
     }
 
     private fun rollDice(){
         val randomInt = (1..6).random()
         val resultText: TextView = findViewById(R.id.result_text)
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultInt = resultText.text.toString().toIntOrNull()
+        if (resultInt != null && resultInt < 6) {
+            resultText.text = (resultInt+1).toString()
+        }
     }
 }
